984e1b08d346fcbf5a7d6de7193d84cc6ebb609b
A-Lian <emilyhaley80111@gmail.com>
2015-03-13 14:48:30 +0800

    Add DUC Preprocees Component Change Abstract class to handle file

--- src/main/tw/edu/ncu/im/Preprocess/PreprocessComponent.java
+++ src/main/tw/edu/ncu/im/Preprocess/PreprocessComponent.java
@@ -1,8 +1,10 @@
 package tw.edu.ncu.im.Preprocess;
 
+import java.io.File;
+
 import edu.uci.ics.jung.graph.Graph;
 
 public abstract class PreprocessComponent<V,E> {
 	IODelegater writer;
-	public abstract Graph<V,E> execute();
+	public abstract Graph<V,E> execute(File doc);
 }
