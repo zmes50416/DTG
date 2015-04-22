d1aaa57cd354855c912cf6abf58d72bbf4d65987
zmes50416 <zmes50416@gmail.com>
2015-04-22 17:33:32 +0800

    adjust KeyTerm's ability for testing & import algorithm of jung

--- src/main/tw/edu/ncu/im/Preprocess/graph/KeyTerm.java
+++ src/main/tw/edu/ncu/im/Preprocess/graph/KeyTerm.java
@@ -1,10 +1,22 @@
 package tw.edu.ncu.im.Preprocess.graph;
-
+/**
+ * For testing only, to prove the graph can work in normal condition
+ * @author TingWen
+ *
+ */
 public class KeyTerm{
 	String name;
 	Double Freq;
 	public KeyTerm() {
 		this.Freq = 1.0;
+	}
+	public KeyTerm(String _name){
+		this();
+		this.name = _name;
+	}
+	public KeyTerm(String _name,double _freq){
+		this.name = _name;
+		this.Freq = _freq;
 	}
 
 	
