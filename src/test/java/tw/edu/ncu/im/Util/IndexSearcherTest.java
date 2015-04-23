f1fbb372fe85d72cf7247d108ce5e420e76d5e14
Dean <zmes50416@gmail.com>
2015-04-23 15:41:53 +0800

    Fix SolrJ dependency problem

--- src/test/java/tw/edu/ncu/im/Util/IndexSearcherTest.java
+++ src/test/java/tw/edu/ncu/im/Util/IndexSearcherTest.java
@@ -1,5 +1,9 @@
 package tw.edu.ncu.im.Util;
 
+import static org.junit.Assert.*;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,22 +12,22 @@ public class IndexSearcherTest {
 	IndexSearcher searcher;
 	@Before
 	public void setUp() throws Exception {
-		try{
 			searcher = new IndexSearcher("http://140.115.82.105/searchweb");
-			IndexSearcher.getService(searcher.ip);
-		}catch(Throwable e){
-			System.out.println(e.getMessage());
-			
-			Throwable[] suppressed = e.getSuppressed();
-			for(Throwable a :suppressed){
-				System.out.println(a.getMessage());
-			}
-		}
 
 	}
 
 	@Test
 	public void test() {
+		HttpSolrClient service = IndexSearcher.getService(searcher.ip);
+		long num =1000;
+		try {
+			num = this.searcher.searchTermSize("Apple");
+		} catch (SolrServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print(num);
+		assertNotNull(service);
 	}
 
 }
