c1a39038dba24c65cdc2af771b44774d065127b8
A-Lian <emilyhaley80111@gmail.com>
2015-04-24 17:33:32 +0800

    修改 過濾長度模組的bug與新增傳入值

--- src/main/tw/edu/ncu/im/Preprocess/Decorator/FilteredTermLengthDecorator.java
+++ src/main/tw/edu/ncu/im/Preprocess/Decorator/FilteredTermLengthDecorator.java
@@ -2,6 +2,8 @@ package tw.edu.ncu.im.Preprocess.Decorator;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import edu.uci.ics.jung.graph.Graph;
 import tw.edu.ncu.im.Preprocess.PreprocessComponent;
@@ -11,24 +13,38 @@ public class FilteredTermLengthDecorator<V,E> extends PreprocessDecorator<V, E> 
 	 * vertexContent :vertex與Term的配對
 	 */
 	HashMap<V, String> vertexContent;
+	
+	/**
+	 * minLength :term之最小長度
+	 */
+	int minLength;
+	
 	/**
 	 * 
 	 * @param _component :原始的元件
 	 * @param content :vertex與Term的配對
 	 */
-	public FilteredTermLengthDecorator(PreprocessComponent<V, E> _component, HashMap<V, String> content) {
+	public FilteredTermLengthDecorator(PreprocessComponent<V, E> _component, HashMap<V, String> content, int _minLength) {
 		super(_component);
 		this.vertexContent = content;
+		this.minLength = _minLength;
 	}
 	
 	@Override
 	public Graph<V, E> execute(File doc) {
+		
 		Graph<V, E> originGraph = this.originComponent.execute(doc);
+		Set<V> removeTerms = new HashSet<V>();
 		for(V node:originGraph.getVertices()){
 			String term = this.vertexContent.get(node);
-			if(term.length() < 3){
-				this.vertexContent.remove(node);
+			if(term.length() < minLength){
+				//this.vertexContent.remove(node);
+				removeTerms.add(node);
 			}
+		}
+		for(V removeTerm:removeTerms){
+			originGraph.removeVertex(removeTerm);
+			this.vertexContent.remove(removeTerm);
 		}
 		return originGraph;
 	}
