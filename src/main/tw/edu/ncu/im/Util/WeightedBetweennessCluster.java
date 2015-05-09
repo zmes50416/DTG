106ce1372e957c28cd920e8a397633db05ad2091
Dean <zmes50416@gmail.com>
2015-05-09 17:19:09 +0800

    betweenesssCluster debug: 解決錯誤繼承屬性的問題，並且正確避免nullPointer的發生

--- src/main/tw/edu/ncu/im/Util/WeightedBetweennessCluster.java
+++ src/main/tw/edu/ncu/im/Util/WeightedBetweennessCluster.java
@@ -1,5 +1,6 @@
 package tw.edu.ncu.im.Util;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,37 +11,45 @@ import edu.uci.ics.jung.algorithms.cluster.WeakComponentClusterer;
 import edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.Pair;
-
+/**
+ * Because JUNG dont support weighted betweennessCluster so we have to override one
+ * @author TingWen
+ *
+ * @param <V>
+ * @param <E>
+ */
 public class WeightedBetweennessCluster<V,E> extends EdgeBetweennessClusterer<V, E> {
 	private Transformer<E,Double> edgeWeightTransformer;
 	private int mNumEdgesToRemove;
     private Map<E, Pair<V>> edges_removed;
     
 	public WeightedBetweennessCluster(int numEdgesToRemove,Transformer<E,Double> edgeTransformer) {
-		super(numEdgesToRemove);
+		super(numEdgesToRemove); //the private int will not be inher so reassign myself
+		this.mNumEdgesToRemove = numEdgesToRemove;
 		this.edgeWeightTransformer = edgeTransformer;
+        this.edges_removed = new LinkedHashMap<E, Pair<V>>();
 	}
 	/**
 	 * override jung betweenness cluster method to fit weight graph 
 	 */
 	@Override
 	public Set<Set<V>> transform(Graph<V,E> graph){
-		if (this.mNumEdgesToRemove < 0 || mNumEdgesToRemove > graph.getEdgeCount()) {
-            throw new IllegalArgumentException("Invalid number of edges passed in.");
+		if (this.mNumEdgesToRemove <= 0 || mNumEdgesToRemove > graph.getEdgeCount()) {
+            throw new IllegalArgumentException("Invalid number of edges passed in. EdgsToRemove:"+this.mNumEdgesToRemove+",Total Edges:"+graph.getEdgeCount());
         }
         
-        edges_removed.clear();
 
         for (int k=0;k<mNumEdgesToRemove;k++) {
             BetweennessCentrality<V,E> bc = new BetweennessCentrality<V,E>(graph,this.edgeWeightTransformer);
             E to_remove = null;
             double score = 0;
-            for (E e : graph.getEdges())
+            for (E e : graph.getEdges()){
                 if (bc.getEdgeScore(e) > score)
                 {
                     to_remove = e;
                     score = bc.getEdgeScore(e);
                 }
+            }
             edges_removed.put(to_remove, graph.getEndpoints(to_remove));
             graph.removeEdge(to_remove);
         }
