8640d236637241d64c03bf2cee1f479e14bef12b
Dean <zmes50416@gmail.com>
2015-05-14 01:24:55 +0800

    Add standford NLP framework for splitting sentences and tagging

---
+++ src/main/tw/edu/ncu/im/Preprocess/Decorator/StandfordPartOfSpeechFiliter.java
@@ -0,0 +1,98 @@
+package tw.edu.ncu.im.Preprocess.Decorator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import tw.edu.ncu.im.Preprocess.PreprocessComponent;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.uci.ics.jung.graph.Graph;
+
+public class StandfordPartOfSpeechFiliter<V,E> extends PreprocessDecorator<V, E> {
+	
+	/** 
+	 * 節點的字詞內容配對保持在此，在<code>execute()</code>以後可以從此得到POS過濾結果
+	 */
+	Map<V,String> vertexResultsTerms = new HashMap<V,String>();
+	Map<V,List<HasWord>> vertextTerms;
+	static MaxentTagger tagger = new MaxentTagger("tagger-models/english-bidirectional-distsim.tagger");// 單字過濾，根據Standford 的POS
+
+	public StandfordPartOfSpeechFiliter(PreprocessComponent<V, E> _component,Map<V,List<HasWord>> _vertextTerms) {
+		super(_component);
+		this.vertextTerms = _vertextTerms;
+	}
+	@Override
+	public Graph<V, E> execute(File doc) {
+		Graph<V, E> docGraph = this.originComponent.execute(doc);
+		List<String> pharses = new ArrayList<>();
+		HashSet<V> nodesToRemove = new HashSet<>();
+
+		for(Entry<V, List<HasWord>> sentencePair:this.vertextTerms.entrySet()){
+	        List<HasWord> sentence = sentencePair.getValue();
+			List<TaggedWord> taggedList = tagger.tagSentence(sentence);
+			pharses.addAll(this.compoundPharse(taggedList));
+			nodesToRemove.add(sentencePair.getKey());
+		}
+		for(V node:nodesToRemove){
+			docGraph.removeVertex(node);
+		}
+		for(String pharse:pharses){
+			V node = this.vertexFactory.create();
+			this.vertexResultsTerms.put(node, pharse);
+			docGraph.addVertex(node);
+		}
+		return docGraph;
+	}
+	/**
+	 * compund words to combined pharse
+	 * @param words words tagged by tagger
+	 * @return all possible pharse
+	 */
+	private List<String> compoundPharse(List<TaggedWord> words) {
+		List<String> pharses = new ArrayList<>();
+		for (int i = 0; i < words.size(); i++) {
+			String key3 = words.get(i).word();
+			if ((words.get(i).tag().contains("NN") || words.get(i).tag().contains("NNP"))) { 	
+				pharses.add(key3);
+				try {// 組合字過濾
+					String key2 = words.get(i-1).word();
+					if (words.get(i-1).tag().equals("JJ")) {
+						String combinePharse = key2 + "+" + key3;
+						pharses.add(combinePharse);
+					} else if (words.get(i-1).tag().contains("NN")
+							|| words.get(i - 1).tag().contains("NNP")) {
+						String key1 = words.get(i-2).word();
+						if (words.get(i-2).tag().contains("NN")
+								|| words.get(i-2).tag().contains("NNP")
+								|| words.get(i-2).tag().equals("JJ")) {
+							String combineThirdPharse = key1 + "+" + key2
+									+ "+" + key3;
+							pharses.add(combineThirdPharse);
+						}
+					}
+				} catch (IndexOutOfBoundsException e) {
+					// 字詞找不到可以連接的更前面字詞,會發生在前1或2的單詞的地方 Do nothing
+				}
+			}
+
+		
+	}
+		return pharses;
+
+	}
+	
+	/**
+	 * @return the vertexResultsTerms
+	 */
+	public Map<V, String> getVertexResultsTerms() {
+		return vertexResultsTerms;
+	}
+		
+
+}
--- src/main/tw/edu/ncu/im/Preprocess/RouterNewsPreprocessor.java
+++ src/main/tw/edu/ncu/im/Preprocess/RouterNewsPreprocessor.java
@@ -2,13 +2,17 @@ package tw.edu.ncu.im.Preprocess;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,6 +20,10 @@ import java.util.regex.Pattern;
 import org.apache.commons.collections15.Factory;
 
 import tw.edu.ncu.im.Preprocess.graph.*;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.UndirectedGraph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
@@ -23,42 +31,44 @@ import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 public class RouterNewsPreprocessor<V,E> extends PreprocessComponent<V,E> {
 	HashSet<KeyTerm> terms;
 	HashSet<TestEdge> edges;
-	HashMap<V,String> vertexContent = new HashMap<>();
+	Map<V,List<HasWord>> vertexContent = new HashMap<>();
 	/**
 	 * @return the vertexContent
 	 */
-	public HashMap<V, String> getVertexContent() {
+	public Map<V,List<HasWord>> getVertexContent() {
 		return vertexContent;
 	}
 	public RouterNewsPreprocessor(Factory<V> _vertexFactory,Factory<E>_edgeFactory){
 		this.vertexFactory = _vertexFactory;
 		this.edgeFactory = _edgeFactory;
 	}
-	@Override
-	public Graph<V,E> execute(File doc) {
-		Graph<V,E> documentGraph = null;
-		if(!doc.isFile()){
-			//TODO 判斷是否為可讀寫且為txt 否則throw Exception
-		}
-		try(BufferedReader r = new BufferedReader(new FileReader(doc));){
-			documentGraph = new UndirectedSparseGraph<V,E>();
-			for(String line=r.readLine();line!=null;line = r.readLine()){
-				line=line.replace("]", "");
-				line=line.replace("[", "");
-				line=line.replace("<", "");
-				line=line.replace(">", "");
 
-		        Matcher m = Pattern.compile("[(),.\"\\?!:;]").matcher(line);
-		        line = m.replaceAll("");
+	@Override
+	public Graph<V, E> execute(File doc) {
+		Graph<V, E> documentGraph = new UndirectedSparseGraph<V, E>();
+		try {
+			List<List<HasWord>> sentences = null;
+			sentences = MaxentTagger.tokenizeText(new FileReader(doc));
+
+			for (List<HasWord> sentence : sentences) {
 				V node = this.vertexFactory.create();
-				this.vertexContent.put(node, line);
+				this.vertexContent.put(node, sentence);
 				documentGraph.addVertex(node);
-				
 			}
-		}catch(IOException e){
+
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return documentGraph;
+
+	}
+	
+	private Set<String> sentence(String line){
+		line=line.replace("]", "");
+		line=line.replace("[", "");
+		line=line.replace("<", "");
+		line=line.replace(">", "");
+		return null;
 		
 	}
 	
